bb7b69375b60b11d209386972250866dc6a44d28
dong1203 <hambur1203@gmail.com>
2025-05-03 18:12:43 +0900

    초과근무 기능 삭제 파일 추가 삭제

--- src/app/overtimes/components/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './ManageOvertimeForm';
-export * from './OvertimeCard';
-export * from './OvertimeRequestCard';
-export * from './OvertimeRequestList';
-export * from './OvertimeHistoryList';
-export * from './TotalOvertimeBox';
-export * from './UsedOvertimeList';
--- src/app/overtimes/request/page.tsx
+++
@@ -1,5 +0,0 @@
-import { ManageOvertimeForm } from '../components';
-
-export default function RequestPointFormPage() {
-  return <ManageOvertimeForm/>;
-}
